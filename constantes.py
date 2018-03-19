2cfeac8a3f8d3b1055fc4b21641f17bca43d2bf8
Timothée CAREL <tcarel@stbarth.lazo>
2018-03-19 09:20:39 +0100

    Deplacement des Images
    Creation de sous dossiers

--- constantes.py
+++ constantes.py
@@ -3,13 +3,13 @@ hauteur_fenetre = 480
 #1920x1080 lycee, 1366x758 maison, 640x480 pour plateformes
 
 
-pgauches = "jamesstatiquegauche.png"
-pgauched1 = "jamesdeplacementgauche1.png"
-pgauched2 = "jamesdeplacementgauche2.png"
-pdroited1 = "jamesdeplacementdroite1.png"
-pdroited2 = "jamesdeplacementdroite2.png"
-pdroites = "jamesstatiquedroite.png"
-phaut = "jamesstatiquedroite.png"
+pgauches = "data/images/jamesstatiquegauche.png"
+pgauched1 = "data/images/jamesdeplacementgauche1.png"
+pgauched2 = "data/images/jamesdeplacementgauche2.png"
+pdroited1 = "data/images/jamesdeplacementdroite1.png"
+pdroited2 = "data/images/jamesdeplacementdroite2.png"
+pdroites = "data/images/jamesstatiquedroite.png"
+phaut = "data/images/jamesstatiquedroite.png"
 
 taille_hero = 31
 largeur_hero = 25
@@ -17,10 +17,10 @@ v_dplc = 6
 gravity = 13
 
 
-bgauche = "tirgauche.png"
-bdroite = "tirdroite.png"
-bhaut = "tirhaut.png"
+bgauche = "data/images/tirgauche.png"
+bdroite = "data/images/tirdroite.png"
+bhaut = "data/images/tirhaut.png"
 
 
 taille_plateforme = 40
-image_plateforme = "plateforme1.png"
+image_plateforme = "data/images/plateforme2.png"
